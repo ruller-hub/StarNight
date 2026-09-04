30188572f6ce21c956bdee830184b785bbb692d9
ruller-hub <jaafarali526@gmail.com>
2026-09-04 23:30:32 +0300

    Update setnickname.js

--- src/commands/Tools/setnickname.js
+++ src/commands/Tools/setnickname.js
@@ -1,1 +1,47 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 
+export default {
+    data: new SlashCommandBuilder()
+        .setName('setnickname')
+        .setDescription('Change a member\'s nickname')
+        .addUserOption(option =>
+            option
+                .setName('user')
+                .setDescription('The member whose nickname you want to change')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('nickname')
+                .setDescription('The new nickname')
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageNicknames),
+
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const nickname = interaction.options.getString('nickname');
+
+        const member = await interaction.guild.members.fetch(user.id);
+
+        if (!member) {
+            return interaction.reply({
+                content: '❌ Member not found.',
+                ephemeral: true
+            });
+        }
+
+        if (!member.manageable) {
+            return interaction.reply({
+                content: '❌ I cannot change this member\'s nickname.',
+                ephemeral: true
+            });
+        }
+
+        await member.setNickname(nickname);
+
+        await interaction.reply({
+            content: `✅ Changed ${user} nickname to **${nickname}**.`
+        });
+    }
+};
