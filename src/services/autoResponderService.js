facc0650f32b0f1fd7f31e9b51d2b11501c75281
ruller-hub <jaafarali526@gmail.com>
2026-09-04 18:57:14 +0300

    Update autoResponderService.js

--- src/services/autoResponderService.js
+++ src/services/autoResponderService.js
@@ -1,8 +1,9 @@
 const autoResponses = {
-  hello: 'Hey 👋',
-  hi: 'Hello!',
-  rules: 'Please check the server rules.',
-  sybau: '😭',
+  link: `_ _
+_ _           ɢᴀᴍᴇ [**StarNight**](https://cdn.discordapp.com/attachments/ex=68c965be&is=68c8143e&hm=b5f95e619dd0a209c3c24dd0bac7eb5e7ae603f37d3db3c9275ac65857c51acf&) [** .ᐟ**](https://discord.gg/star-night) ɢɪᴠᴇᴀᴡᴀʏ
+_ _          __Community__       \`Kurdish\`    __Staff__
+-# _ _                         *starnight for everyone *
+_ _`,
 };
 
 export function getAutoResponse(content) {
